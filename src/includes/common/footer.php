1fd79895931758b0792916a594642bc4cd50c9ee
Nicolás López <nicolopezcesan@gmail.com>
2023-06-26 18:37:50 -0300

    add social networks

--- src/includes/common/footer.php
+++ src/includes/common/footer.php
@@ -3,20 +3,18 @@
     <div class="row">
       <div class="col-sm-12 contact">
         <div class="row">
+
           <div class="col-sm-6 col-md-4">
             <ul>
               <li>
-                <img src="/assets/images/ecosan-logo-footer.svg" alt="Ecosan"
-                     class="logo" />
+                <img src="/assets/images/ecosan-logo-footer.svg" alt="Ecosan" class="logo" />
               </li>
               <li class="contact-item">
-                <img src="/assets/images/call.svg" alt="0810 444 2266"
-                     class="icon" />
+                <img src="/assets/images/call.svg" alt="0810 444 2266" class="icon" />
                 0810 444 2266
               </li>
               <li class="contact-item">
-                <img src="/assets/images/phone.svg" alt="0810 444 2266"
-                     class="icon" />
+                <img src="/assets/images/phone.svg" alt="0810 444 2266" class="icon" />
                 11 4846 0995
               </li>
               <li class="contact-links">
@@ -25,61 +23,83 @@
               </li>
             </ul>
           </div>
+
           <div class="col-sm-6 col-md-4">
             <ul>
-              <li><h2>Institucional</h2></li>
+              <li>
+                <h2>Institucional</h2>
+              </li>
               <li><a href="/nosotros">Nosotros</a></li>
               <li><a href="/construccion">Catalogo</a></li>
               <li><a href="/novedades#prensa">Prensa</a></li>
-              <li><a
-                   href="http://ecosan.com.ar/Politica-de-Calidad-y-Medioambiente.pdf">Política
-                  de Calidad y
-                  Medioambiente</a></li>
+              <li><a href="http://ecosan.com.ar/Politica-de-Calidad-y-Medioambiente.pdf">
+                  Política de Calidad y Medioambiente</a></li>
             </ul>
           </div>
+
 
           <div class="col-sm-8 col-md-4 iso-container">
             <img src="/assets/images/cucardas.svg" alt="ISO" class="iso" />
           </div>
-        
+
+          <div class="col-sm-12 col-md-12 col-lg-12 mt-5 social-networks-container">
+            <span>REDES SOCIALES:</span>
+            <li>
+              <a href="https://www.youtube.com/@ECOSANargentina">
+                <img src="/assets/images/social-networks/youtube.svg" alt="youtube" class="youtube" />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/ecosansa">
+                <img src="/assets/images/social-networks/twitter.svg" alt="twitter" class="twitter" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/ecosansa">
+                <img src="/assets/images/social-networks/instagram.svg" alt="instagram" class="instagram" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/ecosanargentina">
+                <img src="/assets/images/social-networks/facebook.svg" alt="facebook" class="facebook" />
+              </a>
+            </li>
+          </div>
         </div>
       </div>
+
       <div class="col-sm-12 branches-header">
         <div class="col-sm-12 found-branches-container">
 
-          <a data-bs-toggle="collapse" href="#collapseExample" role="button"
-             aria-expanded="false" aria-controls="collapseExample"
-             class="collapsed">
-            <img src="/assets/images/marker.svg" alt="Encontra tu Oficina"
-                 class="icon" />
+          <a data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" class="collapsed">
+            <img src="/assets/images/marker.svg" alt="Encontra tu Oficina" class="icon" />
             BASES OPERATIVAS ECOSAN
-            <img src="/assets/images/arrow-right.svg" alt="arrow"
-                 class="arrow" />
+            <img src="/assets/images/arrow-right.svg" alt="arrow" class="arrow" />
           </a>
 
           <div class="collapse" id="collapseExample">
             <?php include "includes/pages/branches.php"; ?>
           </div>
 
-
         </div>
+
         <div class="col-sm-12 copyright">
           <a href="https://wa.me/5491127505050?">
-            <img src="/assets/images/whatsapp.svg" alt="Whatssapp"
-                 class="whatsapp" /> </a>
+            <img src="/assets/images/whatsapp.svg" alt="Whatssapp" class="whatsapp" />
+          </a>
 
           © 2021 ECOSAN, All Rights Reserved. ECOSAN S.A. - CUIT:
           30-70769684-9 - Buszchiazzo 227, Don Torcuato, Buenos Aires,
           República Argentina. <br>
 
           <div class="col-xs-12" style="text-align: left;">
-			        <a href="http://ecosan.com.ar/alquiler-banos-quimicos.php">Alquiler Baños Quimicos</a> -
-			        <a href="http://ecosan.com.ar/modulos-habitacionales.php">Modulos Habitacionales</a> - 
-			        <a href="http://ecosan.com.ar/oficinas-moviles.php">Oficinas Moviles</a> - 
-			        <a href="http://ecosan.com.ar/construcciones-modulares.php">Construcciones Modulares</a> - 
-			        <a target="_blank" href="Politica-de-Calidad-y-Medioambiente.pdf">Política de Calidad y Medioambiente</a> - 
-			        <a href="http://www.seonet.com.ar">Seo</a> por Seonet
-			    </div>
+            <a href="http://ecosan.com.ar/alquiler-banos-quimicos.php">Alquiler Baños Quimicos</a> -
+            <a href="http://ecosan.com.ar/modulos-habitacionales.php">Modulos Habitacionales</a> -
+            <a href="http://ecosan.com.ar/oficinas-moviles.php">Oficinas Moviles</a> -
+            <a href="http://ecosan.com.ar/construcciones-modulares.php">Construcciones Modulares</a> -
+            <a target="_blank" href="Politica-de-Calidad-y-Medioambiente.pdf">Política de Calidad y Medioambiente</a> -
+            <a href="http://www.seonet.com.ar">Seo</a> por Seonet
+          </div>
         </div>
       </div>
     </div>
