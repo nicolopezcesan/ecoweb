47e54e19930594e5b61407d671b07a0eff6b491f
Nicolás López <nicolopezcesan@gmail.com>
2023-06-28 17:35:09 -0300

    rename to admin user table in mysql

--- src/admin/login/post-login.php
+++ src/admin/login/post-login.php
@@ -6,7 +6,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  $sql = "SELECT * FROM admin_users WHERE username='$username' AND password='$password'";
   $result = $conn->query($sql);
 
   if (mysqli_num_rows($result) > 0) {
