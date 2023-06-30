07d54b46cb00e72be783f0ab669359a5ae16de55
Nicolás López <nicolopezcesan@gmail.com>
2023-06-30 16:19:57 -0300

    add email form contact

---
+++ src/email/contacto-post.php
@@ -0,0 +1,62 @@
+<?php
+
+$name = $_POST['name'];
+$empresa = $_POST['empresa'];
+$telefono = $_POST['telefono'];
+$email = $_POST['email'];
+
+$rubro = $_POST['rubro'];
+$cargo = $_POST['cargo'];
+$pais = $_POST['pais'];
+$comentarios = $_POST['comentarios'];
+
+$body = "
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <title>
+      Presupuesto
+    </title>
+
+    <style>
+      body {
+        font-family: sans-serif;
+        line-height: 12px;
+      }
+      div {
+        padding: 10px 5px;
+      }
+      h2 {
+        padding: 5px 0;
+      }
+    </style>
+  </head>
+  
+  <body>
+    <div>
+      <h2>Datos Personales</h2>
+      <p><b> Nombre y Apellido: </b>" . $name . "</p>
+      <p><b> Empresa: </b>" . $empresa . "</p>
+      <p><b> Teléfono: </b>" . $telefono . "</p>
+      <p><b> Email: </b>" . $email . "</p>
+
+      <p><b> Rubro: </b>" . $rubro . "</p>
+      <p><b> Cargo: </b>" . $cargo . "</p>
+      <p><b> País: </b>" . $pais . "</p>
+      <p><b> Comentarios: </b>" . $comentarios . "</p>
+    </div>
+  </body>
+</html>
+";
+
+echo $body;
+
+// Static data
+$to = "nicolopezcesan@gmail.com"; // info@ecosan.com.ar
+$subject = "Contacto";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: Ecosan <ecosan@consultas.com.ar>' . "\r\n";
+
+mail($to, $subject, $body, $headers);
+
+echo "La consulta fue enviada existosamente.";
