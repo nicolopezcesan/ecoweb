0a1d589446bd8af5982236bc206a0427631ac3f4
Nicolás López <nicolopezcesan@gmail.com>
2023-07-05 16:03:17 -0300

    mysql prod

--- src/mysql/mysql.php
+++ src/mysql/mysql.php
@@ -1,16 +1,43 @@
 <?php
-  ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 
-  $servername = "mysql";
+if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
+  // Local
+  $servername = "localhost";
   $username = "root";
   $password = "root";
   $dbname = "ecosan";
 
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  mysql_set_charset('utf-8');
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+} else {
+
+  // Production
+  $servername = "mysql.globalgate.com.ar";
+  $username = "Jkf76FHa";
+  $password = "MEtwA9Kx";
+  $dbname = "ECO";
+
+  // Ecosan RENTAL
+  /* 
+      $servername = "localhost";
+      $username = "esteba58_ecosan";
+      $password = "sindocs3553";
+      $dbname = "esteba58_ecosan";
+      */
+
+  // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
 
   // Check connection
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-
-?>
+}
