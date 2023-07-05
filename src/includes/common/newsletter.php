67f7af4597cc6afdc325d7818071bf98a524b8e5
Nicolás López <nicolopezcesan@gmail.com>
2023-07-05 15:31:59 -0300

    add styles to newsletter

--- src/includes/common/newsletter.php
+++ src/includes/common/newsletter.php
@@ -3,15 +3,26 @@
     <div class="container">
       <div class="newsletter-container">
         <i class="bi bi-envelope"></i>
-        <h2>Enterate de nuestras novedades</h2>
-        <div class="d-flex">
-          <input type="text" class="form-control"
-                 placeholder="Ingresa tu email acá"
-                 aria-label="Ingresa tu email acá"
-                 aria-describedby="newsletter">
-          <button type="button" class="btn btn-dark">OK</button>
+        <h2 id="title">Enterate de nuestras novedades</h2>
+        <div id="input-container" class="d-flex">
+          <input type="text" class="form-control" placeholder="Ingresa tu email acá" aria-label="Ingresa tu email acá" aria-describedby="newsletter">
+          <button id="suscribe-button" type="button" class="btn btn-dark">OK</button>
         </div>
       </div>
     </div>
   </div>
 </section>
+
+<script>
+  const button = document.querySelector("#suscribe-button");
+  const h2 = document.getElementById("title");
+
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const container = document.getElementById('input-container');
+    container.remove();
+
+    h2.innerHTML = 'Gracias por suscribirte!';
+  });
+</script>
